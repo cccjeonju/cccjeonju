3d4621a7167279f21883e0c927de3e8304139ebb
cccjeonju <31886718+cccjeonju@users.noreply.github.com>
2017-09-14 00:03:33 +0900

    출석하기 기능 구현

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ jQuery(function($){
 			}
 
 			if ($('#phoneCheck').val() != phoneNumber) {
-				alert('등록하신 게 맞습니까?\n\'등록 확인\' 버튼을 눌러 진행하거나 '등록'해주세요.');
+				alert('등록하신 게 맞습니까?\n\'등록 확인\' 버튼을 눌러 진행하거나 \'등록\'해주세요.');
 				$('#submitBtn').prop('disabled', false); // 버튼 활성화 복귀
 				$('#phone').focus();
 				return false;
