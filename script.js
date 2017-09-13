a13a5696cffec8640022ea03c4e00c7a0c74daaa
cccjeonju <31886718+cccjeonju@users.noreply.github.com>
2017-09-13 21:23:05 +0900

    강의목록 가져오기

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ jQuery(function($){
 		//console.log('* SHEET DATA URL - https://goo.gl/Vvge1E'); // 구글 스프레드시트 URL.
 		//console.log(data);
 		//console.log(list);
-		//console.log('* 전체 등록 수: ' + sum + '명');
+		console.log('* 전체 등록 수: ' + sum + '명');
 		for (var i = 0; i < sum; i++) { // 전체 강의 목록을 콘솔에 출력. 
-		    console.log(i+1 + '  ' + list[i].c[4].v + ' / ' + list[i].c[5].v);
-		    $('select').append( $('<option value="' + list[i].c[3].v + '">' + list[i].c[4].v + ' / ' + list[i].c[5].v) + '</option>' );
+		    console.log(i+1 + '  ' + list[i].c[4].v + ' / ' + list[i].c[5].v + ' / ' + list[i].c[6].v);
+		    $('select').append( $('<option value="' + list[i].c[4].v + '">' + list[i].c[5].v + ' / ' + list[i].c[6].v) + '</option>' );
 		}
 		$('.loading-container').fadeOut(); // 로딩바 제거.
 
