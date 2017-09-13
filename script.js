11e6ffede591d54ea2b13b6c1e83292c01528a31
cccjeonju <31886718+cccjeonju@users.noreply.github.com>
2017-09-13 22:00:16 +0900

    수강과목 출력 비교테스트

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ jQuery(function($){
 		for (var i = 0; i < sum; i++) { // 전체 강의 목록을 콘솔에 출력. 
 		    console.log(i+1 + '  ' + list[i].c[4].v.toString() + ' / ' + list[i].c[5].v.toString() + ' / ' + list[i].c[6].v.toString());
 		    
-		    //$('#subject').append( $('<option value="' + list[i].c[4].v.toString() + '">[' + list[i].c[1].v.toString() + '] ' + list[i].c[6].v.toString() + ' / ' + list[i].c[5].v.toString() + '</option>') );
+		    $('#subject').append( $('<option value="' + list[i].c[4].v.toString() + '">[' + list[i].c[1].v.toString() + '] ' + list[i].c[6].v.toString() + ' / ' + list[i].c[5].v.toString() + '</option>') );
 		    $('.subject').append( $('<p><label><input type="radio" name="subject" value="' + list[i].c[4].v.toString() + '">[' + list[i].c[1].v.toString() + '] ' + list[i].c[6].v.toString() + ' / ' + list[i].c[5].v.toString() + '</label></p>') );
 		}
 		$('.loading-container').fadeOut(); // 로딩바 제거.
