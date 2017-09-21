69c1aaf8da0b459377154a72e876a73a799f1a22
cccjeonju <31886718+cccjeonju@users.noreply.github.com>
2017-09-21 22:30:09 +0900

    등록 확인 버튼 수정

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ jQuery(function($){
 
 				} else {
 					//console.log(students[0].c[3].v + ' ' + students[0].c[1].v + ' ' + students[0].c[8].v + '님 (' + students[0].c[5].v + ' ' + students[0].c[6].v.toString().substr(-2) + '학번)');
-					$('output>a').html(students[0].c[1].v + ' ' + students[0].c[8].v + '님 ' + students[0].c[3].v + '<br>' + students[0].c[4].v.toString().substr(0,1) + '학년 / ' + students[0].c[5].v + ' ' + students[0].c[6].v.toString().substr(-2) + '학번\n');
+					$('output>a').html(students[0].c[1].v + ' ' + students[0].c[8].v + '님 ' + students[0].c[3].v + '<br>' + students[0].c[4].v.toString().substr(0,1) + '학년 / ' + students[0].c[5].v + ' ' + students[0].c[6].v.toString().substr(-2) + '학번\n').attr('href','');
 					$('input[name="phoneCheck"]').val(students[0].c[3].v.toString().substr(-4));
 					$('input[name="student_name"]').val(students[0].c[2].v);
 				}
