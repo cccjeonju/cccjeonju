70541b19160a73aed89779c37a12071571fe4bcf
cccjeonju <31886718+cccjeonju@users.noreply.github.com>
2017-09-25 07:24:31 +0900

    테이블 정렬 추가

--- script_list.js
+++ script_list.js
@@ -173,11 +173,9 @@ $(function($){
 		//console.log('today: ' + today);
 
 		$.ajax({
-			// =============================================
-			// Query 조건문 추가해야함 (오늘 날짜)
-			// =============================================
 			type: 'GET',
-			url: 'https://docs.google.com/spreadsheets/d/'+KEY_SPREADSHEET+'/gviz/tq?gid='+GID_SHEET_ATTEND+'&tq=select+*+where+D+matches+\''+subject_code+'\'+and+E+<=+dateTime+\''+today+' 23:59:59\'+and+E+>=+dateTime+\''+today+' 00:00:00\'+order+by+B',
+			// 선택한 과목 중 오늘 날짜에 해당하는 레코드를 가져옴. C column(phone)로 정렬해서
+			url: 'https://docs.google.com/spreadsheets/d/'+KEY_SPREADSHEET+'/gviz/tq?gid='+GID_SHEET_ATTEND+'&tq=select+*+where+D+matches+\''+subject_code+'\'+and+E+<=+dateTime+\''+today+' 23:59:59\'+and+E+>=+dateTime+\''+today+' 00:00:00\'+order+by+C',
 			success: function (data1) {
 				var list_attend = JSON.parse(data1.substring(data1.indexOf('(')+1, data1.indexOf(');'))).table.rows, // 문자열에서 불필요한 부분 제거하고 JSON 형식으로.
 					total_attend = list_attend.length; // 목록 수.
