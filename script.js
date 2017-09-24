fa42b792e2f136df6df4f08d6f7197ea29b4f4c8
cccjeonju <31886718+cccjeonju@users.noreply.github.com>
2017-09-24 15:56:25 +0900

    코드 다듬기

--- script.js
+++ script.js
@@ -38,134 +38,144 @@ $(function($){
 	}).fail(function () {
 		alert('데이터 불러오기 실패. 아마도 jQuery CDN 또는 일시적인 구글 API 문제.');
 	});
-		// --------------------------------------------------
-		// 2-1. '등록 확인'을 최초로 누르면 등록부 시트로부터 해당하는 등록자를 가져와서
-		// 2-2. 기존에 등록된 사람인지 아닌지 표시한다.
-		// --------------------------------------------------
-		var checkID = function() { // 등록자 검색
 
-			phoneNumber = $('#phone').val();
+	// --------------------------------------------------
+	// 2-1. '등록 확인'을 최초로 누르면 등록부 시트로부터 해당하는 등록자를 가져와서
+	// 2-2. 기존에 등록된 사람인지 아닌지 표시한다.
+	// --------------------------------------------------
+	var checkID = function() { // 등록자 검색
 
-			// 전화번호 형식 체크 정규식 010-1234-5678
-			var regExp = /^\d{3}-\d{3,4}-\d{4}$/;
-			if (!regExp.test( phoneNumber )) {
-				alert('010-1234-5678 형식에 맞춰서 핸드폰번호를 넣어주세요.');
-				$('#phone').focus();
-				return false;
+		phoneNumber = $('#phone').val();
+
+		// 전화번호 형식 체크 정규식 010-1234-5678
+		var regExp = /^\d{3}-\d{3,4}-\d{4}$/;
+		if (!regExp.test( phoneNumber )) {
+			alert('010-1234-5678 형식에 맞춰서 핸드폰번호를 넣어주세요.');
+			$('#phone').focus();
+			return false;
+		}
+
+		$.ajax({
+			url: 'https://docs.google.com/spreadsheets/d/'+KEY_SPREADSHEET+'/gviz/tq?gid='+GID_SHEET_REGIST+'&tq=select+*+where+D+matches+\''+phoneNumber+'\''
+		}).done(function (data) {
+			var students = JSON.parse(data.substring(data.indexOf('(')+1, data.indexOf(');'))).table.rows, // 문자열에서 불필요한 부분 제거하고 JSON 형식으로.
+				total = students.length; // 목록 수.
+			
+			console.log('* 검색된 등록자 수: ' + total + '명');
+			
+			$('output').attr('style', 'display:block');
+			if (total<1) {
+				$('output>a').html(phoneNumber + ' 는 아직 등록이 되지 않았습니다.<br>여기를 눌러 \'등록\'을 먼저해주세요.').attr('href', 'https://goo.gl/forms/asSLANctriVD1fjg1');
+
+			} else if (total>1) {
+				alert('동일 아이디 발생! 관리자에게 문의해주시기 바랍니다.');
+			} else {
+				console.log(students[0].c[3].v + ' ' + students[0].c[1].v + ' ' + students[0].c[8].v + '님 (' + students[0].c[5].v + ' ' + students[0].c[6].v.toString().substr(-2) + '학번)');
+				$('output>a').html(students[0].c[1].v + ' ' + students[0].c[8].v + '님 ' + students[0].c[3].v + '<br>' + students[0].c[4].v.toString().substr(0,1) + '학년 / ' + students[0].c[5].v + ' ' + students[0].c[6].v.toString().substr(-2) + '학번\n').attr('href','');
+				$('input[name="phoneCheck"]').val(students[0].c[3].v.toString().substr(-4));
+				$('input[name="student_name"]').val(students[0].c[1].v);
 			}
 
-			$.ajax({
-				url: 'https://docs.google.com/spreadsheets/d/'+KEY_SPREADSHEET+'/gviz/tq?gid='+GID_SHEET_REGIST+'&tq=select+*+where+D+matches+\''+phoneNumber+'\''
-			}).done(function (data) {
-				var students = JSON.parse(data.substring(data.indexOf('(')+1, data.indexOf(');'))).table.rows, // 문자열에서 불필요한 부분 제거하고 JSON 형식으로.
-					total = students.length; // 목록 수.
-				
-				console.log('* 검색된 등록자 수: ' + total + '명');
-				
-				$('output').attr('style', 'display:block');
-				if (total<1) {
-					$('output>a').html(phoneNumber + ' 는 아직 등록이 되지 않았습니다.<br>여기를 눌러 \'등록\'을 먼저해주세요.').attr('href', 'https://goo.gl/forms/asSLANctriVD1fjg1');
+		}).fail(function(){
+			alert('등록자 검색 실패');
+		});
+	};
 
-				} else if (total>1) {
-					alert('동일 아이디 발생');
-				} else {
-					//console.log(students[0].c[3].v + ' ' + students[0].c[1].v + ' ' + students[0].c[8].v + '님 (' + students[0].c[5].v + ' ' + students[0].c[6].v.toString().substr(-2) + '학번)');
-					$('output>a').html(students[0].c[1].v + ' ' + students[0].c[8].v + '님 ' + students[0].c[3].v + '<br>' + students[0].c[4].v.toString().substr(0,1) + '학년 / ' + students[0].c[5].v + ' ' + students[0].c[6].v.toString().substr(-2) + '학번\n').attr('href','');
-					$('input[name="phoneCheck"]').val(students[0].c[3].v.toString().substr(-4));
-					$('input[name="student_name"]').val(students[0].c[2].v);
-				}
+	$('#checkIdBtn').on('click', checkID); // '등록 확인'' 체크
 
-			}).fail(function(){
-				alert('등록자 검색 실패');
-			});
-		};
 
-		// ==================================================
-		// 추가해야할 코드
-		// ==================================================
-		// 교육기간 및 시간이 아닐 경우 출석을 하지 못하도록 하는 코드 (조건문)
-		// 정규식이 맞지 않으면 조회하지 않음
-		// ==================================================
-		$('#checkIdBtn').on('click', checkID); // '등록 확인'' 체크
+	// ==================================================
+	// 추가해야할 코드
+	// ==================================================
+	// 교육기간 및 시간이 아닐 경우 출석을 하지 못하도록 하는 코드 (조건문)
+	// ==================================================
 
-		// --------------------------------------------------
-		// 3-1. '출석하기' 처리
-		// --------------------------------------------------
-		// 1) 데이터 유효성 검사
-		// 
-		// 2) 버튼 비활성화 (중복 등록 방지)
-		//
-		// 3) 출석부 시트에 등록 (timestamp, phone, subject)
-		//    나머지 column은 spreadsheet 안에서 함수로 가져다 쓸 것 (중복데이터 최소화)
 
-		var submitAttend = function() {
-			$('#submitBtn').prop('disabled', true); // 버튼 비활성화
-			$('.loading-container').fadeIn();
+	// --------------------------------------------------
+	// 3-1. '출석하기' 처리
+	// --------------------------------------------------
+	// 1) 데이터 유효성 검사
+	// 
+	// 2) 버튼 비활성화 (중복 등록 방지)
+	//
+	// 3) 출석부 시트에 등록 (timestamp, phone, subject)
+	//    나머지 column은 spreadsheet 안에서 함수로 가져다 쓸 것 (중복데이터 최소화)
+	var submitAttend = function() {
+		$('#submitBtn').prop('disabled', true); // 버튼 비활성화
+		$('.loading-container').fadeIn();
 
-			phoneNumber = $('#phone').val();
+		phoneNumber = $('#phone').val();
 
-			if (!$('#phone').val()) {
-				alert('핸드폰번호를 입력하고 "등록 확인" 버튼을 누르세요.');
-				$('#submitBtn').prop('disabled', false); // 버튼 활성화 복귀
-				$('#phone').focus();
-				return false;
+		if (!phoneNumber) {
+			alert('핸드폰번호를 입력하고 "등록 확인" 버튼을 누르세요.');
+			$('#submitBtn').prop('disabled', false); // 버튼 활성화 복귀
+			$('#phone').focus();
+			return false;
+		}
+
+		// 전화번호 형식 체크 정규식 010-1234-5678
+		var regExp = /^\d{3}-\d{3,4}-\d{4}$/;
+		if (!regExp.test( phoneNumber )) {
+			alert('010-1234-5678 형식에 맞춰서 핸드폰번호를 넣어주세요.');
+			$('#phone').focus();
+			return false;
+		}
+
+		if ($('input[name="phoneCheck"]').val() != phoneNumber.substr(-4)) {
+			alert('등록하신 게 맞습니까?\n"등록 확인" 버튼을 눌러 진행하거나 "등록"해주세요.');
+			$('#submitBtn').prop('disabled', false); // 버튼 활성화 복귀
+			$('#phone').focus();
+			return false;
+		}
+
+		//console.log( $('input[name="phoneCheck"]').val() );
+		//console.log( phoneNumber );
+		//console.log('핸드폰 입력 확인');
+
+		var subject_selected = $(':radio[name="subject"]:checked').val();
+		if (!subject_selected) {
+			alert('수강하는 과목을 선택하세요.');
+			$('#submitBtn').prop('disabled', false); // 버튼 활성화 복귀
+			return false;
+		}
+
+		//console.log('수강과목 선택 확인');
+		//console.log(subject_selected);
+
+		var fee = $('#fee').val();
+		if (fee == '' || fee == null) fee = 0;
+
+		$.ajax({
+			url: WEB_APP_URL + '?sheet_name=' + SHEET_NAME_ATTEND,
+			data: {
+				no: '',
+				attendTime: '',
+				checker: '',
+				phone: phoneNumber,
+				subject: subject_selected,
+				fee: fee
 			}
+		}).done(function(data){
+			var msg = $('input[name="student_name"]').val() + '/' + $('input[name="phoneCheck"]').val('') + '/' + phoneNumber + '/' + subject_selected + '/' + fee;
+			console.log(msg);
+			alert($('input[name="student_name"]').val()+'님 출석이 되었습니다.');
 
-			//console.log( $('input[name="phoneCheck"]').val() );
-			//console.log( phoneNumber );
+			$('#submitBtn').prop('disabled', false);
+			$('#phone').val('');
+			$('#fee').val('');
+			$('input[name="phoneCheck"]').val('');
+			$('output>a').text('');
+			$('output').attr('style', 'display:none');
+			$('input:radio[name="subject"]').prop('checked', false);
+			$('body').scrollTop(0);	// 페이지 맨 위로 이동
+			$('.loading-container').fadeOut();
+		}).fail(function(){
+			alert('출석을 기록하는데 에러가 발생했습니다.');
+			$('#submitBtn').prop('disabled', false); // 버튼 활성화 복귀
+		});
+	};
 
-			if ($('input[name="phoneCheck"]').val() != phoneNumber.substr(-4)) {
-				alert('등록하신 게 맞습니까?\n"등록 확인" 버튼을 눌러 진행하거나 "등록"해주세요.');
-				$('#submitBtn').prop('disabled', false); // 버튼 활성화 복귀
-				$('#phone').focus();
-				return false;
-			}
-
-			//console.log('핸드폰 입력 확인');
-
-			var subject_selected = $(':radio[name="subject"]:checked').val();
-			if (!subject_selected) {
-				alert('수강하는 과목을 선택하세요.');
-				$('#submitBtn').prop('disabled', false); // 버튼 활성화 복귀
-				return false;
-			}
-
-			//console.log('수강과목 선택 확인');
-			//console.log(subject_selected);
-
-			var fee = $('#fee').val();
-			if (fee == '' || fee == null) fee = 0;
-
-			$.ajax({
-				url: WEB_APP_URL + '?sheet_name=' + SHEET_NAME_ATTEND,
-				data: {
-					no: '',
-					attendTime: '',
-					checker: '',
-					phone: phoneNumber,
-					subject: subject_selected,
-					fee: fee
-				}
-			}).done(function(data){
-				var msg = $('input[name="student_name"]').val() + '/' + $('input[name="phoneCheck"]').val('') + '/' + phoneNumber + '/' + subject_selected + '/' + fee;
-				console.log(msg);
-				alert($('input[name="student_name"]').val()+'님 출석이 되었습니다.');
-				$('#submitBtn').prop('disabled', false);
-				$('#phone').val('');
-				$('#fee').val('');
-				$('input[name="phoneCheck"]').val('');
-				$('output>a').text('');
-				$('output').attr('style', 'display:none');
-				$('input:radio[name="subject"]').prop('checked', false);
-				$('body').scrollTop(0);	// 페이지 맨 위로 이동
-				$('.loading-container').fadeOut();
-			}).fail(function(){
-				alert('출석을 기록하는데 에러가 발생했습니다.');
-				$('#submitBtn').prop('disabled', false); // 버튼 활성화 복귀
-			});
-		};
-
-		$('#submitBtn').on('click', submitAttend); 
+	$('#submitBtn').on('click', submitAttend); 
 
 
 });
