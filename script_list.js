dc7cd0b3c9c7187e58146b86aecfac5c7ffb1018
cccjeonju <31886718+cccjeonju@users.noreply.github.com>
2017-09-25 03:56:57 +0900

    Code 정리 list

--- script_list.js
+++ script_list.js
@@ -17,6 +17,19 @@ $(function($){
 
 	var checkerList = new Array();
 
+	// 전체선택 소스
+	$('#checkAll').click(function(){
+		if ( $('#checkAll').prop('checked') ) $('input[name="students"]').prop('checked', true);
+		else $('input[name="students"]').prop('checked',false);
+	});
+
+	// 관리자에게는 날짜 선택을 보여줌
+	if ($('#email').val() == manage_email || $('#email').val() == admin_email ) {
+		$('#checker').before('<input type="date" id="now_date">');
+		$('#now_date').valueAsDate = new Date();
+		//document.getElementById('now_date').valueAsDate = new Date();
+	}
+
 	// --------------------------------------------------
 	// 1-1. 출석체크가 초기 실행되면 개설된 강의 목록을 읽어와야 함
 	// 1-2. 로딩이 끝나면 loadingbar fadeout 효과를 
@@ -24,7 +37,7 @@ $(function($){
 	$.ajax({
 		url: 'https://docs.google.com/spreadsheets/d/'+KEY_SPREADSHEET+'/gviz/tq?gid='+GID_SHEET_SUBJECT
 	}).done(function (data) {
-			var subject_list = JSON.parse(data.substring(data.indexOf('(')+1, data.indexOf(');'))).table.rows, // 문자열에서 불필요한 부분 제거하고 JSON 형식으로.
+		var subject_list = JSON.parse(data.substring(data.indexOf('(')+1, data.indexOf(');'))).table.rows, // 문자열에서 불필요한 부분 제거하고 JSON 형식으로.
 			sum = subject_list.length; // 목록 수.
 
 		//console.log('* SHEET DATA URL - https://docs.google.com/spreadsheets/d/1PHN8N0nY7YLw5NlYTp9VqSvqOHdgsvR2W8BfAZ8AtY4/edit?usp=sharing'); // 구글 스프레드시트 URL.
@@ -49,7 +62,6 @@ $(function($){
 		    if(subject_list[i].c[8] != null) checkerList[i].assist = subject_list[i].c[8].v;
 		}
 		$('.loading-container').fadeOut(); // 로딩바 제거.
-
 	}).fail(function () {
 		alert('데이터 불러오기 실패. 아마도 jQuery CDN 또는 일시적인 구글 API 문제.');
 	});
@@ -58,6 +70,7 @@ $(function($){
 	// 2-1. 과목을 선택했을 때 - 출석부 시트에서 오늘(특정) 날짜가 있고, 선택된 과목의 레코드들을 읽어옴
 	// --------------------------------------------------
 	$('#subjectCode').change(function() { // 과목 바꾸면 리스트 갱신
+
 		$('.loading-container').fadeIn();
 		$('.studentList').empty(); // 명단을 초기화해서 모두 지움
 
@@ -86,11 +99,11 @@ $(function($){
 			url: 'https://docs.google.com/spreadsheets/d/'+KEY_SPREADSHEET+'/gviz/tq?gid='+GID_SHEET_ATTEND+'&tq=select+*+where+D+matches+\''+subject_code+'\'',//+and+E+matches+\''+today+'\''
 			success: function (data1) {
 				var list_attend = JSON.parse(data1.substring(data1.indexOf('(')+1, data1.indexOf(');'))).table.rows, // 문자열에서 불필요한 부분 제거하고 JSON 형식으로.
-					total = list_attend.length; // 목록 수.
+					total_attend = list_attend.length; // 목록 수.
 				//console.log('* SHEET DATA URL - https://docs.google.com/spreadsheets/d/1PHN8N0nY7YLw5NlYTp9VqSvqOHdgsvR2W8BfAZ8AtY4/edit#gid=2098472162'); // 구글 스프레드시트 URL.
-				//console.log('** 출석 체크한 학생 수: ' + total + '명');
+				//console.log('** 출석 체크한 학생 수: ' + total_attend + '명');
 
-				if (total < 1) {
+				if (total_attend < 1) {
 					$('.studentList').append( $('<tr class=""><td colspan="9" class="blankTr">선택한 인원이 없습니다.</td></tr>') );
 					$('.loading-container').fadeOut();
 					return;
@@ -99,72 +112,71 @@ $(function($){
 				// --------------------------------------------------
 				// 2-2. 해당 하는 날짜의 해당 과목 출석 체크한 사람의 정보를 가져옴
 				// --------------------------------------------------
-				for (var j = 0; j < total; j++) {
-				    //console.log('** ' + (j+1) + '  ' + list_attend[j].c[2].v + ' / ' + list_attend[j].c[1].v);
-				    //list_attend[].c[0] = no (row number)
-				    //				c[1] = timestamp (attend time for student)
-				    //				c[2] = phone (student's)
-				    //				c[3] = subject (code)
-				    //				c[4] = checktime (check time for teacher)
-				    //				c[5] = checker (teacher's email)
-				    //				c[6] = fee (student's / just today)
-
-				    attendTime[ii] = list_attend[j].c[4].f,
-				    	 noRow[ii] = list_attend[j].c[0].f;
-				   	 checkTime[ii] =(list_attend[j].c[1] != null) ? list_attend[j].c[1].f : '';
-				   	//  feeToday[ii] =(list_attend[j].c[6] != null) ? ( (list_attend[j].c[6].f!=null)?list_attend[j].c[6].f:'0') : '0';
-				   	  feeToday[ii] =(list_attend[j].c[6] != null) ? list_attend[j].c[6].f : '';
-
-				    $.ajax({
-				    	type: 'GET',
-				    	async: false,
-						url: 'https://docs.google.com/spreadsheets/d/'+KEY_SPREADSHEET+'/gviz/tq?gid='+GID_SHEET_REGIST+'&tq=select+*+where+D+matches+\''+list_attend[j].c[2].v+'\'', // phone number로 user 정보 가져오기
-						success: function(data2) {				    
-							var user = JSON.parse(data2.substring(data2.indexOf('(')+1, data2.indexOf(');'))).table.rows, // 문자열에서 불필요한 부분 제거하고 JSON 형식으로.
-								users = user.length; // 목록 수.
-							//console.log('*** 출석체크 한 사람 중 읽어오기 : ' + users + '명 - 2명 이상일 경우는 데이터 유효성 결여상태');
-
-							// users=1 이어야 하지만 데이터 무결성을 위하여 반복문 사용
-							for (var k = 0; k < users; k++) {
-						    	//console.log('*** '' + (k+1) + '  ' + user[k].c[5].v + ' / ' + user[k].c[4].v + ' / ' + user[k].c[6].v);
-							    //$('.studentList').append( $('<li><label><input type="radio" name="subject" value="' + user[k].c[4].v.toString() + '">[' + user[k].c[1].v.toString() + '] ' + user[k].c[6].v.toString() + ' / ' + user[k].c[5].v.toString() + '</label></li>') );
-							    // user[].c[0] = timestamp
-							    //			1] = name (student's)
-							    //			2] = sex
-							    //			3] = phone
-							    //			4] = grade
-							    //			5] = campus name
-							    //			6] = starting year
-							    //			7] = register
-							    //			8] = title (soonjang)
-							    //			9] = 11/9 subject, [10]=11/16, [11]=11/23
-
-								var studentTr = '<tr class="">\n';
-								studentTr += '<td class="co1"><input type="hidden" name="no" value="'+noRow[ii]+'">\n';
-								studentTr += '<input type="hidden" name="attend_time" value="'+attendTime[ii]+'">\n';
-								if( checkTime[ii] == '' || checkTime[ii] == null) {
-									studentTr += '<input type="checkbox" name="students" value="'+user[k].c[3].v+'">';
-								} else {
-									studentTr += '<button type="button" id="cancelBtn" name="cancel-button">취소</button>';
-								}
-								studentTr += '</td><td class="co2">'+(ii+1)+'</td>\n';
-								studentTr += '<td class="co3">'+user[k].c[1].v+'</td>\n';	// 이름
-								studentTr += '<td class="co4">'+user[k].c[8].v+'</td>\n';	// 호칭
-								studentTr += '<td class="co5">'+user[k].c[2].v.toString().substr(0,1)+'</td>\n';	// 성별
-								studentTr += '<td class="co6">'+user[k].c[4].v.toString().substr(0,1)+'</td>\n';	// 학년
-								studentTr += '<td class="co7">'+user[k].c[5].v.toString().substr(0,5)+'</td>\n';	// 소속
-								studentTr += '<td class="co8">'+user[k].c[6].v.toString().substr(-2) +'</td>\n';	// 학번
-								studentTr += '<td class="co9"><input type="text" name="fee" class="fee" size="7" value="'+feeToday[ii]+'">원</td>\n';	// 회비
-								studentTr += '</tr>\n';
-								ii++;
-								$('.studentList').append( studentTr );
-							}
-						},
-						error: function() {
-				    		alert('출석한 사용자의 정보를 읽어오는데 실패했습니다.');
-				    	}
-				    });
+				for (var j = 0; j < total_attend; j++) {
+					//console.log('** ' + (j+1) + '  ' + list_attend[j].c[2].v + ' / ' + list_attend[j].c[1].v);
+					//list_attend[].c[0] = no (row number)
+					//				c[1] = timestamp (attend time for student)
+					//				c[2] = phone (student's)
+					//				c[3] = subject (code)
+					//				c[4] = checktime (check time for teacher)
+					//				c[5] = checker (teacher's email)
+					//				c[6] = fee (student's / just today)
+					// 현재 유저(출석자)에 대한 정보를 저장
+					noRow[ii] = list_attend[j].c[0].f;
+					attendTime[ii] =(list_attend[j].c[1] != null) ? list_attend[j].c[1].f : list_attend[j].c[4].f;
+					checkTime[ii] =(list_attend[j].c[1] != null) ? list_attend[j].c[1].f : '';
+					feeToday[ii] =(list_attend[j].c[6] != null) ? list_attend[j].c[6].f : '0';
 				}
+
+			    $.ajax({
+			    	type: 'GET',
+					url: 'https://docs.google.com/spreadsheets/d/'+KEY_SPREADSHEET+'/gviz/tq?gid='+GID_SHEET_REGIST+'&tq=select+*+where+D+matches+\''+list_attend[j].c[2].v+'\'', // phone number로 user 정보 가져오기
+					success: function(data2) {				    
+						var user = JSON.parse(data2.substring(data2.indexOf('(')+1, data2.indexOf(');'))).table.rows, // 문자열에서 불필요한 부분 제거하고 JSON 형식으로.
+							users = user.length; // 목록 수.
+						//console.log('*** 출석체크 한 사람 중 읽어오기 : ' + users + '명 - 2명 이상일 경우는 데이터 유효성 결여상태');
+
+						// users=1 이어야 하지만 데이터 무결성을 위하여 반복문 사용
+						for (var k = 0; k < users; k++) {
+					    	//console.log('*** '' + (k+1) + '  ' + user[k].c[5].v + ' / ' + user[k].c[4].v + ' / ' + user[k].c[6].v);
+						    //$('.studentList').append( $('<li><label><input type="radio" name="subject" value="' + user[k].c[4].v.toString() + '">[' + user[k].c[1].v.toString() + '] ' + user[k].c[6].v.toString() + ' / ' + user[k].c[5].v.toString() + '</label></li>') );
+						    // user[].c[0] = timestamp
+						    //			1] = name (student's)
+						    //			2] = sex
+						    //			3] = phone
+						    //			4] = grade
+						    //			5] = campus name
+						    //			6] = starting year
+						    //			7] = register
+						    //			8] = title (soonjang)
+						    //			9] = 11/9 subject, [10]=11/16, [11]=11/23
+
+							var studentTr = '<tr class="">\n';
+							studentTr += '<td class="co1"><input type="hidden" name="no" value="'+noRow[ii]+'">\n';
+							studentTr += '<input type="hidden" name="attend_time" value="'+attendTime[ii]+'">\n';
+							if( checkTime[ii] == '' || checkTime[ii] == null) {
+								studentTr += '<input type="checkbox" name="students" value="'+user[k].c[3].v+'">';
+							} else {
+								studentTr += '<button type="button" id="cancelBtn" name="cancel-button">취소</button>';
+							}
+							studentTr += '</td><td class="co2">'+(ii+1)+'</td>\n';
+							studentTr += '<td class="co3">'+user[k].c[1].v+'</td>\n';	// 이름
+							studentTr += '<td class="co4">'+user[k].c[8].v+'</td>\n';	// 호칭
+							studentTr += '<td class="co5">'+user[k].c[2].v.toString().substr(0,1)+'</td>\n';	// 성별
+							studentTr += '<td class="co6">'+user[k].c[4].v.toString().substr(0,1)+'</td>\n';	// 학년
+							studentTr += '<td class="co7">'+user[k].c[5].v.toString().substr(0,5)+'</td>\n';	// 소속
+							studentTr += '<td class="co8">'+user[k].c[6].v.toString().substr(-2) +'</td>\n';	// 학번
+							studentTr += '<td class="co9"><input type="text" name="fee" class="fee" size="7" value="'+feeToday[ii]+'">원</td>\n';	// 회비
+							studentTr += '</tr>\n';
+							ii++;
+							$('.studentList').append( studentTr );
+						}
+					},
+					error: function() {
+			    		alert('출석한 사용자의 정보를 읽어오는데 실패했습니다.');
+			    	}
+			    });
+
 				$('.loading-container').fadeOut();
 
 				// 출석 확인 버튼 생성
@@ -186,19 +198,6 @@ $(function($){
 			}
 		});
 	}); // changeSubject {}
-
-
-	// 전체선택 소스
-	$('#checkAll').click(function(){
-		if ( $('#checkAll').prop('checked') ) $('input[name="students"]').prop('checked', true);
-		else $('input[name="students"]').prop('checked',false);
-	});
-
-	if ($('#email').val() == manage_email || $('#email').val() == admin_email ) {
-		$('#checker').before('<input type="date" id="now_date">');
-		$('#now_date').valueAsDate = new Date();
-		//document.getElementById('now_date').valueAsDate = new Date();
-	}
 
 	// --------------------------------------------------
 	// 3-1. '출석확인' 처리
