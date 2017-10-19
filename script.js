acaf4508429b8322f7693042c20643718d4e9484
cccjeonju <31886718+cccjeonju@users.noreply.github.com>
2017-10-19 22:47:08 +0900

    출석부 시트 필드 순서 변경 적용

--- script.js
+++ script.js
@@ -75,15 +75,15 @@ $(function($){
 					msg += ' (' + students[0].c[5].v;	// campus
 					msg += ' ' + students[0].c[6].v.toString().substr(-2) + '학번)';
 					msg += ' ' + students[0].c[1].v.toString().substr(0,1) + '학년';
-					msg += '/ 등록비: ' + students[0].c[13].f;
+					msg += ' / 등록비: ' + students[0].c[13].f;
 				console.log(msg);
 				msg = students[0].c[2].v + ' ';		// name
-				msg	+= students[0].c[4].v + '님 ';	// title (soonjang)
-				msg += students[0].c[8].v + '<br>';	// phone
+				msg	+= students[0].c[8].v + '님 ';	// title (soonjang)
+				msg += students[0].c[4].v + '<br>';	// phone
 				msg += students[0].c[1].v.toString().substr(0,1) + '학년 / ';
 				msg += students[0].c[5].v + ' ';	// campus
-				msg += students[0].c[6].v.toString().substr(-2) + '학번 / 회비(어제): ';
-				msg += students[0].c[13].f + '\n';
+				msg += students[0].c[6].v.toString().substr(-2) + '학번 / 회비(어제까지): ';
+				msg += students[0].c[13].f + '원\n';
 				$('output>a').html(msg).removeAttr('href');
 				$('input[name="phoneCheck"]').val(students[0].c[3].v.toString().substr(-4));
 				$('input[name="studentName"]').val(students[0].c[1].v);
